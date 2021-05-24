139838d07427d1fbd6f56662da19e67c597e9126
Mauhwi <tykityki7@gmail.com>
2021-05-25 02:01:22 +0500

    start

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="portfolio">
+      <h1>Портфолио</h1>
+      <div class="container">
+          <div class="inner-container">
+            <div class="card"></div>
+          </div>
+          <div class="controls">
+              
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Portfolio",
+    methods: {
+      
+    }
+}
+</script>
+
+<style>
+    .portfolio {
+      padding-top: 60px;
+      padding-bottom: 60px;
+    }
+</style>
